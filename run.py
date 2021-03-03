e1364f7cc2bf16b531448c5a8955a27cbf196d2c
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 14:22:18 +0100

    render data collected from internet

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-print("test")
+import matplotlib.pyplot as plt
+import numpy as np
 
-print("another thing")
+data = np.genfromtxt("Benitoite__R050320__Broad_Scan__780__0__unoriented__Raman_Data_RAW__17845.txt", comments="##", delimiter=",")
+
+range = data[:,0]
+shift = data[:,1]
+
+max_shift = max(abs(shift))
+shift = shift / max_shift
+
+plt.plot(range, shift)
+plt.show()

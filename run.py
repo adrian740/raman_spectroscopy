1d78c8c1975f544a8f6206a802a7b04b61740c6d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 16:17:01 +0100

    update

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from BaselineRemoval import BaselineRemoval
 
-data = np.genfromtxt("Benitoite__R050320__Broad_Scan__780__0__unoriented__Raman_Data_RAW__17845.txt", comments="##", delimiter=",")
+data = np.genfromtxt("Benitoite__R050320__Broad_Scan__532__0__unoriented__Raman_Data_RAW__17844.txt", comments="##", delimiter=",")
 
 range = data[:,0]
 shift = data[:,1]
@@ -9,5 +10,10 @@ shift = data[:,1]
 max_shift = max(abs(shift))
 shift = shift / max_shift
 
+# See https://pypi.org/project/BaselineRemoval/
+dat = BaselineRemoval(shift)
+Zhangfit_output=dat.ZhangFit()
+
 plt.plot(range, shift)
+plt.plot(range, Zhangfit_output)
 plt.show()

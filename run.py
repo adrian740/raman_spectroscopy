e26f36d31fff207e5edae070700c46b8301e86da
BuildTools <unconfigured@null.spigotmc.org>
2021-03-05 16:14:45 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as signal
+from os import listdir
+from os.path import isfile, join
 
-file = "data analysis project//120oC/120 80_300__X_70__Y_-2.35417__Time_1162.txt"
+path = "data analysis project//120oC//"
 
-data = np.genfromtxt(file, delimiter="\t")
+allfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-range = data[:,0]
-shift = data[:,1]
+def read_data(filename):
+    data = np.genfromtxt(filename, delimiter="\t")
 
-plt.plot(range, shift, label="Raw Data")
+    range = data[:, 0]
+    shift = data[:, 1]
+
+    N = 3  # Filter order
+    Wn = 0.1  # Cutoff frequency
+    B, A = signal.butter(N, Wn, output='ba')
+    smooth_data = signal.filtfilt(B, A, shift)
+
+    return range, shift, smooth_data
+
+i = 0
+skip = 20
+
+for filename in allfiles:
+    if filename.endswith(".txt"):
+        i += 1
+        if i % skip is not 0:
+            continue
+        range_, raw_data, smooth_data = read_data(path + filename)
+        plt.plot(range_, smooth_data, label=filename)
 
 plt.legend()
 plt.show()

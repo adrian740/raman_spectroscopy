7e7d9b016e281b4c64e4313b7c736546c36f6b1a
Adrian <burnobostock@gmail.com>
2021-03-11 14:49:40 +0100

    Update test.py

--- test.py
+++ test.py
@@ -18,14 +18,19 @@ def read_data(filename):
     # raman shift, amplitude, + smoothed amplitude
     return shift, intensity, smooth_data, float(filename.split("_")[-7])
 
-file = "data analysis project//120oC//120 80_0__X_-50__Y_-2.35417__Time_0.txt"
-#file = "data analysis project//120oC//120 80_298__X_69.2__Y_-2.35417__Time_1154.txt"
-shift, intensity, smooth, x_coord = read_data(file)
+def plot_data(filename):
+    shift, intensity, smooth, x_coord = read_data(filename)
+    plt.plot(shift, intensity, label="x = " + str(x_coord))
 
 max_v, min_v = 5000, 0
 plt.plot([1005, 1005], [min_v, max_v], color="r", label="PEI Peak")
 plt.plot([985, 985], [min_v, max_v], color="g", label="EPO Peak")
 
-plt.plot(shift, intensity, label="x = " + str(x_coord))
+plot_data("data analysis project//120oC//120 80_0__X_-50__Y_-2.35417__Time_0.txt")
+plot_data("data analysis project//120oC//120 80_298__X_69.2__Y_-2.35417__Time_1154.txt")
+
+plt.ylabel("Intensity [units]")
+plt.xlabel(r"Raman Shift [$cm^{-1}$]")
+
 plt.legend()
 plt.show()
